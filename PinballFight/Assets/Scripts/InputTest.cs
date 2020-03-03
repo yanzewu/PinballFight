b3b113bb06b699dbfef6bbf71acef21813217954
Yanze Wu <wuyanze0@gmail.com>
2020-03-02 22:20:28 -0500

    Initial commit

---
+++ PinballFight/Assets/Scripts/InputTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputTest : MonoBehaviour
+{
+    GameObject obj;
+    GameObject output;
+    Dictionary<int, GameObject> pairs = new Dictionary<int, GameObject>();
+    public bool is_enabled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GameObject.Find("Sample");
+        output = GameObject.Find("Text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!is_enabled) return;
+
+        foreach (Touch touch in Input.touches){
+
+            if (touch.phase == TouchPhase.Began){
+                
+                var world_pos = Camera.main.ScreenToWorldPoint(touch.position);
+                world_pos.z = 0;
+                output.GetComponent<Text>().text = world_pos.ToString() + ":" + touch.fingerId.ToString();
+                
+                pairs[touch.fingerId] = Instantiate(obj, world_pos, Quaternion.identity);
+                pairs[touch.fingerId].GetComponent<InputTest>().is_enabled = false;
+            }
+            else if (touch.phase == TouchPhase.Moved){
+                var world_pos = Camera.main.ScreenToWorldPoint(touch.position);
+                world_pos.z = 0;
+                output.GetComponent<Text>().text = touch.fingerId.ToString();
+
+                if (pairs.ContainsKey(touch.fingerId)){
+                    pairs[touch.fingerId].transform.position = world_pos;
+                }
+            }
+        }
+        
+    }
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+The Unity version required is 2019.2.11f1
+
+Please notify me if you want another version.
+
+See PinballFight/Assets/README.txt for details.
